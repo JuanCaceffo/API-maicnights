ca9afd906bcb62118b1f96bb7b58434afc9c35c3
JuanCaceffo <caceffojuan@gmail.com>
2024-03-23 22:09:53 -0300

    agregado de extension method a entero para lanzar error si es negativo

--- src/main/kotlin/ar/edu/unsam/phm/magicnightsback/domain/Helpers.kt
+++ src/main/kotlin/ar/edu/unsam/phm/magicnightsback/domain/Helpers.kt
@@ -1,6 +1,14 @@
 package ar.edu.unsam.phm.magicnightsback.domain
 
+import ar.edu.unsam.phm.magicnightsback.error.BusinessException
 import java.time.LocalDate
 import java.time.Period
 
 fun LocalDate.calculateAge(): Int = Period.between(this, LocalDate.now()).years
+
+fun Int.throwErrorIfNegative(error: RuntimeException): Int {
+    if(this < 0){
+        throw error
+    }
+    return this
+}
