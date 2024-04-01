fca352a9f9aa00046ecf1973d8c101a14029ed53
JuanCaceffo <caceffojuan@gmail.com>
2024-04-01 16:29:22 -0300

    agregado de endpoint para obtener todos los tickets del carrito de un usuario + arreglo de problema de tipos en userRepository

---
+++ src/test/kotlin/ar/edu/unsam/phm/magicnightsback/controller/UserControllerTest.kt
@@ -0,0 +1,14 @@
+package ar.edu.unsam.phm.magicnightsback.controller;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisplayName("Controller de usuario")
+class UserControllerTest() {
+}
