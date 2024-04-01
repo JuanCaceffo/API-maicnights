77fc27e325f784c7bceecbe893b8e8ba913ee5aa
lopez-sol <lopez.msol.07@gmail.com>
2024-04-01 13:24:00 -0300

    agregado de elementos al boostrap
    Co-authored-by: MADESCOCES <MADESCOCES@GMAIL.COM>

--- src/main/kotlin/ar/edu/unsam/phm/magicnightsback/domain/Ticket.kt
+++ src/main/kotlin/ar/edu/unsam/phm/magicnightsback/domain/Ticket.kt
@@ -1,13 +1,15 @@
 package ar.edu.unsam.phm.magicnightsback.domain
 
 import ar.edu.unsam.phm.magicnightsback.repository.Iterable
-import java.time.LocalDate
 
 data class Ticket(
-    val showId: Long,
-    val date: LocalDate,
-    val price: Double
+    val show: Show,
+    val showDate: ShowDate,
+    val seatType: SeatTypes,
 ) : Iterable() {
+
+    val price: Double = show.fullTicketPrice(seatType)
+
     override fun validSearchCondition(value: String): Boolean {
         TODO("Not yet implemented")
     }
