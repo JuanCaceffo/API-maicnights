e4c08c0b913e598accaabcf6a483ea46703f7219
lopez-sol <lopez.msol.07@gmail.com>
2024-05-11 13:14:22 -0300

    simplificacion de Facility y manejo de asientos

--- src/main/kotlin/ar/edu/unsam/phm/magicnightsback/domain/Facility.kt
+++ src/main/kotlin/ar/edu/unsam/phm/magicnightsback/domain/Facility.kt
@@ -1,5 +1,6 @@
 package ar.edu.unsam.phm.magicnightsback.domain
 
+import ar.edu.unsam.phm.magicnightsback.data.constants.ColumnLength
 import ar.edu.unsam.phm.magicnightsback.error.BusinessException
 import ar.edu.unsam.phm.magicnightsback.error.FacilityError
 import jakarta.annotation.Nullable
@@ -9,41 +10,53 @@ import jakarta.persistence.*
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "type")
 abstract class Facility(
-    @Column(length = 100)
+    @Column(length = ColumnLength.MEDIUM, nullable = false, unique = true)
     val name: String,
     @Embedded
     val location: Point
 ) {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long? = null
+    var id: Long = 0
 
-    @ManyToMany(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
-    val places: MutableSet<Place> = mutableSetOf()
+    var cost = 0.0
 
     abstract var fixedPrice: Double
-    abstract fun fixedCostVariant(): Double
-    fun cost() = fixedPrice + fixedCostVariant()
+    fun fixedCostVariant(): Double = 0.0
 
-    fun addPlace(seat: SeatTypes, capacity: Int) {
-        validateSeatType(seat.name)
-        places.add(Place(seat, capacity = capacity))
+    @ElementCollection
+    @CollectionTable(
+        name = "seats",
+        joinColumns = [JoinColumn(name = "facility_id", referencedColumnName = "id")]
+    )
+    @MapKeyColumn(name = "type")
+    @Column(name = "quantity")
+    val seats = mutableMapOf<SeatTypes, Int>()
+
+    fun setSeatCapacity(seat: SeatTypes, capacity: Int) {
+        validateSeatType(seat)
+        seats[seat] = capacity
     }
+
+    fun updateCost() {
+        cost = fixedPrice + fixedCostVariant()
+    }
+
+    fun seatCapacityOf(seat: SeatTypes) = seats[seat]
 
     //TODO: validar si el asiento existe dentro de la facility
-    fun getPlaceBySeatName(seatName: String): Place {
-        validateSeatType(seatName)
-        return places.find { it.seatType.name == seatName }!!
-    }
-    fun getPlaceCapacity(seat: SeatTypes) = getPlaceBySeatName(seat.name).capacity
+//    fun getPlaceBySeatName(seatName: String): Place {
+//        validateSeatType(seatName)
+//        return places.find { it.seatType.name == seatName }!!
+//    }
+//    fun getPlaceCapacity(seat: SeatTypes) = getPlaceBySeatName(seat.name).capacity
 
-    fun getTotalSeatCapacity() = places.sumOf { it.capacity }
+//    fun getTotalSeatCapacity() = places.sumOf { it.capacity }
 
-    abstract fun validSeatTypes(): List<String>
+    abstract fun validSeatTypes(): List<SeatTypes>
 
-    fun validateSeatType(name: String) {
-        if (name !in validSeatTypes()) {
-            throw BusinessException(FacilityError.INVALID_SEAT_TYPE)
+    private fun validateSeatType(seat: SeatTypes) {
+        if (seat !in validSeatTypes()) {
+            throw BusinessException(CreationError.INVALID_SEAT_TYPE(seat.name))
         }
     }
 }
@@ -60,18 +73,21 @@ class Stadium(
         require(fixedPrice >= 0) { throw BusinessException(FacilityError.NEGATIVE_PRICE) }
     }
 
-    override fun validSeatTypes() = listOf(SeatTypes.BOX.name, SeatTypes.UPPERLEVEL.name, SeatTypes.FIELD.name)
+    override fun validSeatTypes() = listOf(SeatTypes.BOX, SeatTypes.UPPERLEVEL, SeatTypes.FIELD)
 
     override fun fixedCostVariant(): Double = 0.0
 }
 
 @Entity
 @DiscriminatorValue("Theater")
-class Theater(name: String, location: Point) : Facility(name, location) {
-    override fun validSeatTypes() = listOf(SeatTypes.PULLMAN.name, SeatTypes.LOWERLEVEL.name)
-
+class Theater(
+    name: String,
+    location: Point,
     @Nullable
     var hasGoodAcoustics: Boolean = false
+) : Facility(name, location) {
+    override fun validSeatTypes() = listOf(SeatTypes.PULLMAN, SeatTypes.LOWERLEVEL)
+
     override var fixedPrice: Double = 100000.0
     override fun fixedCostVariant(): Double = if (hasGoodAcoustics) 50000.0 else 0.0
 }
