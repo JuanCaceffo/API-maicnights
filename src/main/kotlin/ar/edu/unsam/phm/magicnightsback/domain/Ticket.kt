625ab41d0ac7596ef814d6b207f06ff7166735fa
madescoces <madescoces@gmail.com>
2024-05-11 13:26:17 -0300

    ...

--- src/main/kotlin/ar/edu/unsam/phm/magicnightsback/domain/Ticket.kt
+++ src/main/kotlin/ar/edu/unsam/phm/magicnightsback/domain/Ticket.kt
@@ -20,3 +20,22 @@ data class Ticket(
     @Column(nullable = true)
     var date: LocalDateTime? = null
 }
+
+
+//@Entity
+//data class Ticket(
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    val show: Show,
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    val showDate: ShowDate,
+//    @Enumerated(EnumType.STRING)
+//    val seat: SeatTypes,
+//    @Column
+//    val quantity: Int = 1,
+//) {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    var id: Long = 0
+//    val price = show.ticketPrice(seat) * quantity
+//
+//}
