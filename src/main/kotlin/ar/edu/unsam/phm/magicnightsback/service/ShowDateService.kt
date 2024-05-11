5e75b3a92327e6981f46cbf9afc6123a203275da
lopez-sol <lopez.msol.07@gmail.com>
2024-05-11 12:37:16 -0300

    Service hecho

--- src/main/kotlin/ar/edu/unsam/phm/magicnightsback/service/ShowDateService.kt
+++ src/main/kotlin/ar/edu/unsam/phm/magicnightsback/service/ShowDateService.kt
@@ -1,18 +1,23 @@
 package ar.edu.unsam.phm.magicnightsback.service
 
+import ar.edu.unsam.phm.magicnights.utils.stringMe
+import ar.edu.unsam.phm.magicnightsback.domain.ShowDate
+import ar.edu.unsam.phm.magicnightsback.domain.User
+import ar.edu.unsam.phm.magicnightsback.repository.ShowDateRepository
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 import java.util.*
+import kotlin.jvm.optionals.getOrNull
 
 @Service
 class ShowDateService(
     private val showDateRepository: ShowDateRepository
 ) {
     @Transactional(Transactional.TxType.NEVER)
-    fun findShoDateByUUID(uuid: UUID): ShowDate? =
+    fun findShoDateByUUID(uuid: Long): ShowDate? =
         showDateRepository.findById(uuid).getOrNull()
 
     @Transactional(Transactional.TxType.NEVER)
-    fun findShowDateOrErrorByUUID(uuid: UUID): ShowDate =
-        findShoDateByUUID(uuid) ?: throw ResponseFindException(FindError.NOT_FOUND(uuid, User::class.stringMe()))
+    fun findShowDateOrErrorByUUID(uuid: Long): ShowDate =
+        findShoDateByUUID(uuid) ?: throw ResponseFindException (FindError.NOT_FOUND(uuid, User::class.stringMe()))
 }
---
+++ src/main/kotlin/ar/edu/unsam/phm/magicnightsback/utils/Date.kt
@@ -0,0 +1,14 @@
+package ar.edu.unsam.phm.magicnights.utils
+
+import ar.edu.unsam.phm.magicnights.exceptions.CreationError
+import ar.edu.unsam.phm.magicnights.exceptions.IllegalArgumentException
+import java.time.LocalDateTime
+
+fun LocalDateTime.validateIsNotBefore(): LocalDateTime {
+    if (this.isBefore(LocalDateTime.now())) {
+        throw IllegalArgumentException(CreationError.DATE_NOT_PASSED)
+    }
+    return this
+}
+
+fun fromNow(minutes: Int) = System.currentTimeMillis() + minutes * 60 * 1000
