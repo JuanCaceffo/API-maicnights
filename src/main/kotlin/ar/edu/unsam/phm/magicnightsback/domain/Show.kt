3bf023570be6c12534b77fdacadb16fb2555da06
Denise <deniseamarfil@gmail.com>
2024-05-19 03:31:45 -0300

    Se cambian clases Show y ShowDate para que persistan en mongo

--- src/main/kotlin/ar/edu/unsam/phm/magicnightsback/domain/Show.kt
+++ src/main/kotlin/ar/edu/unsam/phm/magicnightsback/domain/Show.kt
@@ -1,117 +1,24 @@
 package ar.edu.unsam.phm.magicnightsback.domain
 
-import ar.edu.unsam.phm.magicnightsback.data.constants.ColumnLength
 import ar.edu.unsam.phm.magicnightsback.domain.enums.Rentability
 import ar.edu.unsam.phm.magicnightsback.utils.removeSpaces
-import jakarta.persistence.*
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 
-//@Entity
-//class Show(
-//    @Column(length = 40)
-//    var name: String,
-//
-//    @ManyToOne
-//    var band: Band,
-//
-//    @ManyToOne
-//    var facility: Facility,
-//) {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    var id: Long = 0
-//
 
-//
-//    @OneToMany(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
-//    var dates = mutableSetOf<ShowDate>()
-//
-//    @Enumerated(EnumType.STRING)
-//    @Column(length = 40)
-//    var rentabilityType: Rentability = Rentability.BASE_PRICE
-//
-//    @ElementCollection(fetch = FetchType.LAZY)
-//    val pendingAttendeesIds = mutableListOf<Long>()
-//    fun geoLocationString() = facility.location.toGeolocation()
-//
-//    // Cost methods
-//    fun baseCost(): Double = (band.cost) + (facility.cost())
-//    fun baseTicketPrice(seat: SeatTypes): Double =
-//        (facility.let { baseCost() / it.getTotalSeatCapacity() }) + seat.price
-//
-//    fun ticketPrice(seat: SeatTypes): Double = (baseTicketPrice(seat) * rentabilityType.factor).truncate()
-//    fun allTicketPrices() = facility.places.map { ticketPrice(it.seatType) }
-//
-//    // ShowDate methods
-//    fun getSeatTypes() = facility.places.map { it }
-//    fun getShowDateById(showDateId: Long) =
-//        dates.find { it.id == showDateId } ?: throw NotFoundException(ShowDateError.MSG_DATE_NOT_FOUND)
-//
-//    fun initialDates(newDates: List<LocalDateTime>) {
-//        newDates.forEach {
-//            dates.add(ShowDate(it, this))
-//        }
-//    }
-//
-//    fun addDate(date: LocalDateTime): ShowDate {
-//        validNewDate(date.toLocalDate())
-//        val showDate = ShowDate(date, this)
-//        dates.add(showDate)
-//        return showDate
-//    }
-//
-//    // Admin Methods
-//    fun sales(): List<Double> = facility.places.map { ticketPrice(it.seatType) * totalTicketsSoldOf(it.seatType) }
-//    fun totalSales(): Double = sales().sumOf { it }
-//    fun totalTicketsSoldOf(seat: SeatTypes) = dates.sumOf { it.getReservedSeatsOf(seat) }
-//    fun totalTicketsSold() = getSeatTypes().sumOf { totalTicketsSoldOf(it.seatType) }
-//    fun totalPendingAttendees() = pendingAttendeesIds.size
-//    fun rentability() = (((totalSales() - baseCost()) / totalSales()) * 100).coerceAtLeast(0.0)
-//    fun changeRentability(newShowStatus: Rentability) {
-//        this.rentabilityType = newShowStatus
-//    }
-//
-//    fun getAllStats(show: Show) = PivotStats.stats.map { it.getStat(show) }
-//
-//    //Friends Methods
-//    fun friendsAttendeesProfileImages(user: User): List<String> = friendsAttending(user).map { it.profileImgUrl }
-//
-//    fun friendsAttending(user: User) = allAttendees().filter { user.isMyFriend(it) }
-//
-//    // Dates Methods
-//    fun allDates() = dates.map { it.date }.toList().sortedBy { it }
-//    fun allDatesWithIds() = dates.map { it.toShowDateDTO() }.sortedBy { it.date }
-//    fun allAttendees() = dates.flatMap { it.attendees }.toSet()
-//    fun soldOutDates() = dates.filter { it.isSoldOut() }.size
-//    fun newDateAvailable(show: Show) = PivotStats.stats.all { it.newDateCondition(show) }
-//
-//    //Validations
-//    fun validNewDate(date: LocalDate) {
-//        if (date.isBefore(LocalDate.now())) throw BusinessException(ShowDateError.INVALID_DATE)
-//        if (dates.any { it.date.toLocalDate() == date }) throw BusinessException(ShowDateError.DATE_ALREADY_EXISTS)
-//        if (!newDateAvailable(this)) throw BusinessException(ShowDateError.NEW_SHOW_INVALID_CONDITIONS)
-//    }
-//}
-
-@Entity
+@Document("Shows")
 data class Show(
-    @Column(nullable = false, length = ColumnLength.MEDIUM)
     val name: String,
 
-    @ManyToOne
+    @Transient
     val band: Band,
 
-    @ManyToOne
+    @Transient
     val facility: Facility
 ) {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0
-
-    @Enumerated(EnumType.STRING)
-    @Column(length = ColumnLength.SMALL)
+    lateinit var id: String
     var rentability = Rentability.BASE_PRICE
-
-    @Column(length = 100)
     var imgUrl = "${band.name.removeSpaces().lowercase()}.jpg"
 
     // Seat methods
@@ -125,4 +32,3 @@ data class Show(
         rentability = newRentability
     }
 }
-
--- src/main/kotlin/ar/edu/unsam/phm/magicnightsback/domain/ShowDate.kt
+++ src/main/kotlin/ar/edu/unsam/phm/magicnightsback/domain/ShowDate.kt
@@ -1,72 +1,17 @@
 package ar.edu.unsam.phm.magicnightsback.domain
 
-import ar.edu.unsam.phm.magicnightsback.exceptions.BadArgumentException
-import ar.edu.unsam.phm.magicnightsback.exceptions.BusinessException
-import ar.edu.unsam.phm.magicnightsback.exceptions.CreationError
-import ar.edu.unsam.phm.magicnightsback.exceptions.FindError
-import ar.edu.unsam.phm.magicnightsback.utils.notNegative
-import jakarta.persistence.*
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
 
-//@Entity
-//class ShowDate(
-//    @Column
-//    val date: LocalDateTime,
-//    @ManyToOne
-//    private val show: Show
-//) {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    var id: Long = 0
-//
-//    @ManyToMany(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
-//    val attendees = mutableSetOf<User>()
-//
-//    @ElementCollection(fetch = FetchType.LAZY)
-//    val reservedSeats = show.facility.validSeatTypes().associateWith { 0 }.toMutableMap()
-//
-//    fun addAttendee(user: User) {
-//        attendees.add(user)
-//    }
-//
-//    fun reserveSeat(seat: SeatTypes, quantity: Int) {
-//        quantity.throwIfGreaterThan(availableSeatsOf(seat), ShowDateError.EXCEEDED_CAPACITY)
-//        reservedSeats[seat.name] = (reservedSeats[seat.name]!! + quantity)
-//    }
-//
-//    fun releaseSeat(seat: SeatTypes, quantity: Int) {
-//        reservedSeats[seat.name] = (reservedSeats[seat.name]!! - quantity)
-//    }
-//
-//    fun getReservedSeatsOf(seat: SeatTypes) = reservedSeats[seat.name] ?: 0
-//
-//    fun getAllReservedSeats() = reservedSeats.map { it.value }.sum()
-//
-//    fun availableSeatsOf(seat: SeatTypes): Int {
-//        return show.facility.getPlaceCapacity(seat) - getReservedSeatsOf(seat)
-//    }
-//
-//    fun totalAvailableSeats(): Int {
-//        return show.facility.getTotalSeatCapacity() - getAllReservedSeats()
-//    }
-//
-//    fun datePassed(): Boolean = date.isBefore(LocalDateTime.now())
-//
-//    fun isAttendee(user: User) = attendees.any { it.id == user.id }
-//
-//    fun isSoldOut() = totalAvailableSeats() == 0
-//}
 
-@Entity
+@Document("ShowDates")
 data class ShowDate(
-    @ManyToOne
     val show: Show,
-
     val date: LocalDateTime
 ) {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0
+    val id: String = "0"
 
     // Availability
     fun beenStaged(): Boolean = date.isBefore(LocalDateTime.now())
