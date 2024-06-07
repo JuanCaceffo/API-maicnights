c7b73b5a21d62d9d08fe6f0bad9078db5b46f835
madescoces <madescoces@gmail.com>
2024-06-06 21:45:03 -0300

    Hasta aca llego el amor

--- Docker/magicnights/scripts/rs-init.sh
+++ Docker/magicnights/scripts/rs-init.sh
@@ -4,23 +4,28 @@ DELAY=25
 
 mongosh <<EOF
 var config = {
-    "_id": "mnrs",
+    "_id" : "mnrs",
     "version": 1,
-    "members": [
+    "members" : [
         {
-            "_id": 0,
-            "host": "172.16.238.10:27017",
-            "priority": 2
+            "_id" : 0,
+            "host" : "mongo1:27017",
+            "priority" : 2
         },
         {
-            "_id": 1,
-            "host": "172.16.238.11:27017",
-            "priority": 1
+            "_id" : 1,
+            "host" : "mongo2:27017",
+            "priority" : 1
         },
         {
-            "_id": 2,
-            "host": "172.16.238.12:27017",
-            "priority": 1
+            "_id" : 2,
+            "host" : "mongo3:27017",
+            "priority" : 1
+        },
+        {
+            "_id" : 3,
+            "host" : "mongo4:27017",
+            "arbiterOnly" : true
         }
     ]
 };
@@ -31,4 +36,4 @@ echo "** Waiting for ${DELAY} seconds for replicaset configuration to be applied
 
 sleep $DELAY
 
-mongosh mongodb://172.16.238.10:27017/admin < /scripts/init.js
+mongosh mongodb://mongo1:27017/admin < /scripts/init.js
